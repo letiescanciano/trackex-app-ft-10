e9b7e3228d7b1d1a90552118f3dca02777773dcc
letiescanciano <leticia.escanciano@gmail.com>
2022-01-18 18:00:30 +0100

    M7_S2 - Transaction List

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import styled from 'styled-components'
+import data from './data'
 
+import { Edit, DeleteForever } from '@mui/icons-material'
 const Container = styled.nav`
   height: 100vh;
 `
@@ -26,8 +29,48 @@ const Item = styled.li`
   }
 `
 
+const Table = styled.table`
+  width: 100%;
+  text-align: left;
+  padding: 64px;
+`
+
+const HeadCell = styled.td`
+  padding: 16px 0;
+  width: 20%;
+`
+
+const TableCell = styled.td`
+  padding: 8px 0;
+  width: 20%;
+`
+const AmountCell = styled.td`
+  color: ${props => {
+    return props.type === 'expense' ? '#FF7661' : '#00E4C6'
+  }};
+`
+const ActionsCell = styled.td`
+  display: flex;
+  column-gap: 16px;
+`
 const App = () => {
   console.log('App is being rendered')
+  const [transactions, setTransactions] = useState([])
+  console.log('transactions', transactions)
+
+  useEffect(() => {
+    console.log('useEffect first render')
+    setTransactions(data)
+
+    // setTimeout(() => {
+    //   console.log('Information received from server....')
+    //   setTransactions(data)
+    // }, 5000)
+  }, [])
+
+  // useEffect(() => {
+  //   console.log('useEffect transactions')
+  // }, [transactions])
   return (
     <div className='layout'>
       <Container>
@@ -49,8 +92,48 @@ const App = () => {
         </List>
       </Container>
 
-      <main>
-        <h1>main</h1>
+      <main style={{ width: '100%' }}>
+        <h1>Transaction List</h1>
+        <Table>
+          <thead>
+            <tr>
+              <HeadCell>Date</HeadCell>
+              <HeadCell>Name</HeadCell>
+              <HeadCell>Category</HeadCell>
+              <HeadCell>Amount</HeadCell>
+              <HeadCell>Type</HeadCell>
+              <HeadCell></HeadCell>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <TableCell>{transaction.date}</TableCell>
+                  <TableCell>{transaction.name}</TableCell>
+                  <TableCell>{transaction.category}</TableCell>
+                  <AmountCell type={transaction.type}>
+                    {transaction.amount}
+                  </AmountCell>
+                  <TableCell>{transaction.type}</TableCell>
+                  <ActionsCell>
+                    <Edit
+                      onClick={() => {
+                        console.log('Edit transaction')
+                      }}
+                    />
+                    <DeleteForever
+                      style={{ color: '#FF7661' }}
+                      onClick={() => {
+                        console.log('Delete transaction')
+                      }}
+                    />
+                  </ActionsCell>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
       </main>
     </div>
   )
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    id: 0,
+    date: '22/04/2021',
+    name: 'Coffee',
+    category: 'eating_out',
+    amount: 4.5,
+    type: 'expense',
+  },
+  {
+    id: 1,
+    date: '22/04/2021',
+    name: 'Coffee',
+    category: 'eating_out',
+    amount: 4.5,
+    type: 'expense',
+  },
+  {
+    id: 2,
+    date: '22/04/2021',
+
+    name: 'April payroll',
+    category: 'salary',
+    amount: 4500,
+    type: 'income',
+  },
+  {
+    id: 3,
+    date: '22/04/2021',
+
+    name: 'Coffee',
+    category: 'eating_out',
+    amount: 4.5,
+    type: 'expense',
+  },
+  {
+    id: 4,
+    date: '22/04/2021',
+    name: 'T-shirt',
+    category: 'clothes',
+    amount: 4.5,
+    type: 'expense',
+  },
+  {
+    id: 5,
+    date: '22/04/2021',
+    name: 'Macbook',
+    category: 'eating_out',
+    amount: 4.5,
+    type: 'expense',
+  },
+]
+
+export default data
