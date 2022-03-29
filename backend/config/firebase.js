4518a28d5a7175bcd695b7e342978ca9c772078f
letiescanciano <leticia.escanciano@gmail.com>
2022-03-29 18:21:30 +0200

    Deploy > update firebase config private key

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin')
-
+// console.log(JSON.stringify(process.env.FIREBASE_PRIVATE_KEY))
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 })
