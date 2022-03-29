01944bb51423f59a2ad3f9a70cc1f5ac1d767b26
letiescanciano <leticia.escanciano@gmail.com>
2022-03-29 17:43:39 +0200

    Deploy > update firebase config

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,11 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = require('./serviceAccountKey.json')
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 })
 
 module.exports = admin
